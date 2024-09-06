1786782b6dbad7d5a123da609967ea61f52780a4
elvis-onobo <elvis.onobo@gmail.com>
2024-09-06 16:12:06 +0100

    declare a base class

---
+++ oop/classes.py
@@ -0,0 +1,16 @@
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def make_sound(self):
+        return f"{self.name} is making a sound"
+
+    def walk(self):
+        return f"{self.name} is walking"
+    
+
+cat = Animal("Bullet")
+print(cat.make_sound())
+print(cat.name)
+print(cat.walk())
