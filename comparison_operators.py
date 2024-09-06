859e026a457ce54e4cc9934582835a96ddec824a
elvis-onobo <elvis.onobo@gmail.com>
2024-09-06 12:05:27 +0100

    comparison operators

---
+++ comparison_operators.py
@@ -0,0 +1,10 @@
+x = 6
+y = 7
+
+# comparison operations
+print(x == y)  # Outputs: False
+print(x != y)  # Outputs: True
+print(x > y)   # Outputs: False
+print(x < y)   # Outputs: True
+print(x >= y)  # Outputs: False
+print(x <= y)  # Outputs: True
