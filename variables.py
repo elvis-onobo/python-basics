f8b5a07959acdc109d54751cdfaa3aebc73d780f
elvis-onobo <elvis.onobo@gmail.com>
2024-09-06 12:03:33 +0100

    variables

---
+++ variables.py
@@ -0,0 +1,6 @@
+name = "Alice"
+age = 30
+height = 5.9
+is_student = True # False
+
+print(f"My name is {name}, it is {is_student} that I am a student. I am {age} years old and my height is {height}")
