a08d5f15d64d480cba59c41374fa4811b80c9530
elvis-onobo <elvis.onobo@gmail.com>
2024-09-06 12:10:43 +0100

    logical operators

---
+++ logical_operators.py
@@ -0,0 +1,12 @@
+a = 5
+b = None
+
+if a and b:
+    print("Both a and b have values")
+
+
+if a or b:
+    print("At least one of a or b has a value")
+
+if not a or not b:
+    print("Neither a nor b has a value")
