6a3a56b327f4cd8aac5e60fa7938289eca412fd6
elvis-onobo <elvis.onobo@gmail.com>
2024-09-06 12:03:46 +0100

    math operators

---
+++ basic_math_operators.py
@@ -0,0 +1,16 @@
+
+sum = 5 + 6
+subtract = 6-2
+multiply = 8 * 5
+divide = 6/2
+floor_divide = 7//3
+modulus = 7 % 3
+exponential = 2**3
+
+print(f"sum: {sum}")
+print(f"subtract: {subtract}")
+print(f"multiply: {multiply}")
+print(f"divide: {divide}")
+print(f"floor_divide: {floor_divide}")
+print(f"modulus: {modulus}")
+print(f"exponential: {exponential}")
